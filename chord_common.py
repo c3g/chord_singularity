23698e9ac3ad2377e3a0d26c628b0051a88143a5
David Lougheed <david.lougheed@gmail.com>
2019-08-07 16:06:22 -0400

    Add config var for location of chord env file

--- chord_common.py
+++ chord_common.py
@@ -8,6 +8,8 @@ from typing import Callable, Dict, List
 
 def get_config_vars(s: Dict) -> Dict:
     return {
+        "CHORD_ENV": "/chord/tmp/env",  # TODO: Should this be in tmp?
+
         "SERVICE_SOCKET": f"/chord/tmp/{s['id']}.sock",
         "SERVICE_VENV": f"/chord/services/{s['id']}/env",
         "SERVICE_BASE_URL": f"/api/{s['id']}",
