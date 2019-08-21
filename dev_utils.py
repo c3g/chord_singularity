c2e1d46425367cd46bed59a45d289584ff16f839
David Lougheed <david.lougheed@gmail.com>
2019-08-21 16:14:47 -0400

    Fix creation of temporary folder after it's needed

--- dev_utils.py
+++ dev_utils.py
@@ -8,12 +8,14 @@ import subprocess
 def action_start(args):
     for i in range(1, args.cluster_size + 1):
         print(f"[CHORD DEV UTILS] Starting instance {i}...")
+
+        subprocess.run(["mkdir", "-p", f"/tmp/chord/{i}"])
+
         with open(f"/tmp/chord/{i}/env", "w") as f:
             f.write(f"export CHORD_URL=http://{i}.chord.dlougheed.com/\n")
             f.write("export CHORD_REGISTRY_URL=http://1.chord.dlougheed.com/\n")
 
         user_dir = os.path.expanduser("~")
-        subprocess.run(["mkdir", "-p", f"/tmp/chord/{i}"])
         subprocess.run(["mkdir", "-p", os.path.join(user_dir, f"chord_data/{i}")])
         subprocess.run(["singularity", "instance", "start",
                         "--bind", f"/tmp/chord/{i}:/chord/tmp",
