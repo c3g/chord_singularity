d87f91d6215c7b7f95176376713e1dc96042d7c2
David Lougheed <david.lougheed@gmail.com>
2019-10-09 13:06:59 -0400

    Add postgres port config var [skip ci]

--- container_scripts/chord_common.py
+++ container_scripts/chord_common.py
@@ -30,6 +30,7 @@ def get_config_vars(s: Dict, services_config_path: str) -> Dict:
 
             "POSTGRES_SOCKET": "/chord/tmp/postgresql/.s.PGSQL.5433",
             "POSTGRES_SOCKET_DIR": "/chord/tmp/postgresql",
+            "POSTGRES_PORT": "5433",
             "POSTGRES_DATABASE": f"{s['id']}_db",
             "POSTGRES_USER": f"{s['id']}_acct",
             "POSTGRES_PASSWORD": generate_secret_key(),  # Generate a password to be used for the Postgres user
