683e4098f15b18d63e1f75f7bdf987d119d708ac
David Lougheed <david.lougheed@gmail.com>
2019-12-04 18:27:42 -0500

    Wait for pkill to occur in non_wsgi stop script

--- container_scripts/container_non_wsgi_stop.py
+++ container_scripts/container_non_wsgi_stop.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import time
 import subprocess
 import sys
 
@@ -7,6 +8,9 @@ from typing import Dict, List
 
 # noinspection PyUnresolvedReferences
 from chord_common import get_runtime_config_vars, get_env_str, main
+
+SLEEP_TIME = 0.5
+MAX_WAIT_ITERATIONS = 10 / SLEEP_TIME
 
 
 def job(services: List[Dict], services_config_path: str):
@@ -17,9 +21,18 @@ def job(services: List[Dict], services_config_path: str):
         config_vars = get_runtime_config_vars(s, services_config_path)
 
         try:
-            subprocess.run(f"/bin/bash -c 'pkill -9 -F "
-                           f"{config_vars['SERVICE_TEMP']}/{config_vars['SERVICE_ARTIFACT']}.pid'",
-                           shell=True, check=True)
+            pid_file = f"{config_vars['SERVICE_TEMP']}/{config_vars['SERVICE_ARTIFACT']}.pid"
+            subprocess.run(f"/bin/bash -c 'pkill -9 -F {pid_file}'", shell=True, check=True)
+
+            max_wait = MAX_WAIT_ITERATIONS
+            while max_wait > 0:
+                if subprocess.run(f"/bin/bash -c 'kill -0 \"$(cat {pid_file})\"'", shell=True,
+                                  stderr=subprocess.DEVNULL).returncode == 1:
+                    break
+
+                time.sleep(SLEEP_TIME)
+                max_wait -= 1
+
         except subprocess.CalledProcessError:
             print(f"Error stopping service {config_vars['SERVICE_ARTIFACT']}", file=sys.stderr)
 
