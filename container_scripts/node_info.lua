b13ffa8608f95a326951b07155fcb375e7c6150b
David Lougheed <david.lougheed@gmail.com>
2020-01-27 16:11:15 -0500

    Serve OIDC_DISCOVERY_URI in node info endpoint

--- container_scripts/node_info.lua
+++ container_scripts/node_info.lua
@@ -2,12 +2,17 @@
 
 local cjson = require("cjson")
 
+local auth_file = assert(io.open(ngx.var.chord_auth_config))
+local auth_params = cjson.decode(auth_file:read("*all"))
+auth_file:close()
+
 local config_file = assert(io.open(ngx.var.chord_instance_config))
 local config_params = cjson.decode(config_file:read("*all"))
 config_file:close()
 
 local response = {
-  CHORD_URL=config_params["CHORD_URL"]
+  CHORD_URL=config_params["CHORD_URL"],
+  OIDC_DISCOVERY_URI=auth_params["OIDC_DISCOVERY_URI"],
 }
 
 ngx.status = 200
