a8cf5358941ea90ff490b376da1446243b654e2b
David Lougheed <david.lougheed@gmail.com>
2020-02-12 10:43:27 -0500

    Rename postgres version env var in start_script

--- container_scripts/start_script.bash
+++ container_scripts/start_script.bash
@@ -2,7 +2,7 @@
 
 # Script to start various processes for the CHORD system in order.
 
-CPG="11"
+POSTGRES_VERSION="11"
 
 # Remove any stray socket files
 rm -f /chord/tmp/*.sock
@@ -34,18 +34,18 @@ mkdir -p /chord/data/postgresql
 echo "Starting Postgres..."
 
 # Set up boot log in a writable location if it has not been set up already
-if [[ ! -f /chord/tmp/postgresql/postgresql-${CPG}-main.log ]]; then
-  touch /chord/tmp/postgresql/postgresql-${CPG}-main.log
+if [[ ! -f /chord/tmp/postgresql/postgresql-${POSTGRES_VERSION}-main.log ]]; then
+  touch /chord/tmp/postgresql/postgresql-${POSTGRES_VERSION}-main.log
 fi
 
 # Initialize DB if nothing's there, then start the cluster
 database_created=False
 if [[ ! "$(ls -A /chord/data/postgresql)" ]]; then
-  /usr/lib/postgresql/${CPG}/bin/initdb -D /chord/data/postgresql &> /dev/null
+  /usr/lib/postgresql/${POSTGRES_VERSION}/bin/initdb -D /chord/data/postgresql &> /dev/null
   database_created=True
 fi
 
-pg_ctlcluster ${CPG} main start
+pg_ctlcluster ${POSTGRES_VERSION} main start
 
 
 echo "Running pre-start operations..."
