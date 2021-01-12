7be53fdc5b0174f7576ba5ba5409ea3cff3da211
David Lougheed <david.lougheed@gmail.com>
2021-01-11 19:36:07 -0500

    Change how session secrets are handled slightly

--- container_scripts/generate_session_secret.bash
+++ container_scripts/generate_session_secret.bash
@@ -2,6 +2,6 @@
 
 # Script to (re-)generate a secure session secret for a Bento instance.
 #  - must be called before NGINX is started
-#  - the value in the file should be copied into the NGINX conf template
+#  - the value should be copied into the NGINX conf template
 
-openssl rand -hex 48 > /chord/tmp/.session_secret
+openssl rand -hex 48
